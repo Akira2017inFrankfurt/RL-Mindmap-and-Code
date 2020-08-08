16d09b6522810735e6899c3a7d3817e167ca12ce
Akira2017inFrankfurt <liangindeutschland@gmail.com>
2020-08-08 22:46:43 +0200

    Create test_pg.py
    因为一开始没有发现错误的原因：
    InvalidArgumentError: Broadcast dimension mismatch. Operands could not be broadcast together with the shape of X = [16, 2] and the shape of Y = [16, 4, 2]. Received [2] in X is not equal to [4] in Y at i:1.
      [Hint: Expected x_dims_array[i] == y_dims_array[i] || x_dims_array[i] <= 1 || y_dims_array[i] <= 1 == true, but received x_dims_array[i] == y_dims_array[i] || x_dims_array[i] <= 1 || y_dims_array[i] <= 1:0 != true:1.] at (/paddle/paddle/fluid/operators/elementwise/elementwise_op_function.h:160)
      [operator < elementwise_mul > error]
    
    推测是某个张量的维度出现错误。

---
+++ Codes/test_pg.py
@@ -0,0 +1,168 @@
+import gym
+import numpy as np
+
+import paddle.fluid as fluid
+import parl
+from parl import layers
+from parl.utils import logger
+
+LEARNING_RATE = 0.001
+
+
+class Model(parl.Model):
+    def __init__(self, act_dim):
+        act_dim = act_dim
+        hid1_size = 10 * act_dim
+
+        self.fc1 = layers.fc(size=hid1_size, act='tanh')
+        self.fc2 = layers.fc(size=act_dim, act='softmax')
+
+    def forward(self, obs):
+        out = self.fc1(obs)
+        out = self.fc2(out)
+        return out
+
+
+class PolicyGradient(parl.Algorithm):
+    def __init__(self, model, lr=None):
+        self.model = model
+        assert isinstance(lr, float)
+        self.lr = lr
+
+    def predict(self, obs):
+        return self.model(obs)
+
+    def learn(self, obs, action, reward):
+        act_prob = self.model(obs)
+        # log_prob = layers.cross_entropy(act_prob, action)
+        log_prob = layers.reduce_sum(
+            -1.0 * layers.log(act_prob) * layers.one_hot(
+                action, act_prob.shape[1]),
+            dim=1)
+        cost = log_prob * reward
+        cost = layers.reduce_mean(cost)
+
+        optimizer = fluid.optimizer.Adam(self.lr)
+        optimizer.minimize(cost)
+        return cost
+
+
+class Agent(parl.Agent):
+    def __init__(self, algorithm, obs_dim, act_dim):
+        self.obs_dim = obs_dim
+        self.act_dim = act_dim
+        super(Agent, self).__init__(algorithm)
+
+    def build_program(self):
+        self.pred_program = fluid.Program()
+        self.learn_program = fluid.Program()
+
+        with fluid.program_guard(self.pred_program):
+            obs = layers.data(name='obs', shape=[self.obs_dim],
+                              dtype='float32')
+            self.act_prob = self.alg.predict(obs)
+
+        with fluid.program_guard(self.learn_program):
+            obs = layers.data(name='obs', shape=[self.obs_dim],
+                              dtype='float32')
+            act = layers.data(name='act', shape=[1], dtype='int64')
+            reward = layers.data(name='reward', shape=[], dtype='float32')
+            self.cost = self.alg.learn(obs, act, reward)
+
+    def sample(self, obs):
+        obs = np.expand_dims(obs, axis=0)
+        act_prob = self.fluid_executor.run(
+            self.pred_program,
+            feed={'obs': obs.astype('float32')},
+            fetch_list=[self.act_prob])[0]
+        act_prob = np.squeeze(act_prob, axis=0)
+        act = np.random.choice(range(self.act_dim), p=act_prob)
+        return act
+
+    def predict(self, obs):
+        obs = np.expand_dims(obs, axis=0)
+        act_prob = self.fluid_executor.run(
+            self.pred_program,
+            feed={'obs': obs.astype('float32')},
+            fetch_list=[self.act_prob])[0]
+        act_prob = np.squeeze(act_prob, axis=0)
+        act = np.argmax(act_prob)
+        return act
+
+    def learn(self, obs, act, reward):
+        act = np.expand_dims(act, axis=-1)
+        feed = {
+            'obs': obs.astype('float32'),
+            'act': act.astype('int64'),
+            'reward': reward.astype('float32')
+        }
+        cost = self.fluid_executor.run(self.learn_program, feed=feed, fetch_list=[self.cost])[0]
+        return cost
+
+
+def run_episode(env, agent):
+    obs_list, action_list, reward_list = [], [], []
+    obs = env.reset()
+
+    while True:
+        obs_list.append(obs)
+
+        action = agent.sample(obs)
+        action_list.append(action)
+
+        obs, reward, done, info = env.step(action)
+        reward_list.append(reward)
+
+        if done:
+            break
+    return obs_list, action_list, reward_list
+
+
+def evaluate(env, agent, render=False):
+    eval_reward = []
+    for i in range(5):
+        obs = env.reset()
+        episode_reward = 0
+        while True:
+            action = agent.predict(obs)
+            obs, reward, isOver, _ = env.step(action)
+            episode_reward += reward
+
+            if render:
+                env.render()
+            if isOver:
+                break
+
+        eval_reward.append(episode_reward)
+    return np.mean(eval_reward)
+
+
+def calc_reward_to_go(reward_list, gamma=1.0):
+    for i in range(len(reward_list) - 2, -1, -1):
+        reward_list[i] += gamma * reward_list[i + 1]
+    return np.array(reward_list)
+
+
+env = gym.make('CartPole-v0')
+obs_dim = env.observation_space.shape[0]
+act_dim = env.action_space.n
+logger.info('obs_dim {}, act_dim {}'.format(obs_dim, act_dim))
+
+model = Model(act_dim=act_dim)
+alg = PolicyGradient(model, lr=LEARNING_RATE)
+agent = Agent(alg, obs_dim=obs_dim, act_dim=act_dim)
+
+for i in range(1000):
+    obs_list, action_list, reward_list = run_episode(env, agent)
+    if i % 10 == 0:
+        logger.info("Episode {}, Reward {}".format(
+            i, sum(reward_list)))
+
+    batch_obs = np.array(obs_list)
+    batch_action = np.array(action_list)
+    batch_reward = calc_reward_to_go(reward_list)
+
+    agent.learn(batch_obs, batch_obs, batch_reward)
+    if (i + 1) % 100 == 0:
+        total_reward = evaluate(env, agent, render=False)
+        logger.info('Test reward: {}'.format(total_reward))

aa0e092831d4d245caf7c6daaeab624fbcb053f4
Akira2017inFrankfurt <liangindeutschland@gmail.com>
2020-08-08 22:51:12 +0200

    Update test_pg.py
    the right version

--- Codes/test_pg.py
+++ Codes/test_pg.py
@@ -12,7 +12,7 @@ LEARNING_RATE = 0.001
 class Model(parl.Model):
     def __init__(self, act_dim):
         act_dim = act_dim
-        hid1_size = 10 * act_dim
+        hid1_size = act_dim * 10
 
         self.fc1 = layers.fc(size=hid1_size, act='tanh')
         self.fc2 = layers.fc(size=act_dim, act='softmax')
@@ -58,25 +58,26 @@ class Agent(parl.Agent):
         self.learn_program = fluid.Program()
 
         with fluid.program_guard(self.pred_program):
-            obs = layers.data(name='obs', shape=[self.obs_dim],
-                              dtype='float32')
+            obs = layers.data(
+                name='obs', shape=[self.obs_dim], dtype='float32')
             self.act_prob = self.alg.predict(obs)
 
-        with fluid.program_guard(self.learn_program):
-            obs = layers.data(name='obs', shape=[self.obs_dim],
-                              dtype='float32')
+        with fluid.program_guard(
+                self.learn_program):
+            obs = layers.data(
+                name='obs', shape=[self.obs_dim], dtype='float32')
             act = layers.data(name='act', shape=[1], dtype='int64')
             reward = layers.data(name='reward', shape=[], dtype='float32')
             self.cost = self.alg.learn(obs, act, reward)
 
     def sample(self, obs):
-        obs = np.expand_dims(obs, axis=0)
+        obs = np.expand_dims(obs, axis=0) 
         act_prob = self.fluid_executor.run(
             self.pred_program,
             feed={'obs': obs.astype('float32')},
             fetch_list=[self.act_prob])[0]
-        act_prob = np.squeeze(act_prob, axis=0)
-        act = np.random.choice(range(self.act_dim), p=act_prob)
+        act_prob = np.squeeze(act_prob, axis=0) 
+        act = np.random.choice(range(self.act_dim), p=act_prob) 
         return act
 
     def predict(self, obs):
@@ -86,7 +87,7 @@ class Agent(parl.Agent):
             feed={'obs': obs.astype('float32')},
             fetch_list=[self.act_prob])[0]
         act_prob = np.squeeze(act_prob, axis=0)
-        act = np.argmax(act_prob)
+        act = np.argmax(act_prob) 
         return act
 
     def learn(self, obs, act, reward):
@@ -96,17 +97,16 @@ class Agent(parl.Agent):
             'act': act.astype('int64'),
             'reward': reward.astype('float32')
         }
-        cost = self.fluid_executor.run(self.learn_program, feed=feed, fetch_list=[self.cost])[0]
+        cost = self.fluid_executor.run(
+            self.learn_program, feed=feed, fetch_list=[self.cost])[0]
         return cost
 
 
 def run_episode(env, agent):
     obs_list, action_list, reward_list = [], [], []
     obs = env.reset()
-
     while True:
         obs_list.append(obs)
-
         action = agent.sample(obs)
         action_list.append(action)
 
@@ -117,29 +117,26 @@ def run_episode(env, agent):
             break
     return obs_list, action_list, reward_list
 
-
 def evaluate(env, agent, render=False):
     eval_reward = []
     for i in range(5):
         obs = env.reset()
         episode_reward = 0
         while True:
-            action = agent.predict(obs)
+            action = agent.predict(obs) 
             obs, reward, isOver, _ = env.step(action)
             episode_reward += reward
-
             if render:
                 env.render()
             if isOver:
                 break
-
         eval_reward.append(episode_reward)
     return np.mean(eval_reward)
 
 
 def calc_reward_to_go(reward_list, gamma=1.0):
     for i in range(len(reward_list) - 2, -1, -1):
-        reward_list[i] += gamma * reward_list[i + 1]
+        reward_list[i] += gamma * reward_list[i + 1]  # Gt
     return np.array(reward_list)
 
 
@@ -155,14 +152,14 @@ agent = Agent(alg, obs_dim=obs_dim, act_dim=act_dim)
 for i in range(1000):
     obs_list, action_list, reward_list = run_episode(env, agent)
     if i % 10 == 0:
-        logger.info("Episode {}, Reward {}".format(
+        logger.info("Episode {}, Reward Sum {}.".format(
             i, sum(reward_list)))
 
     batch_obs = np.array(obs_list)
     batch_action = np.array(action_list)
     batch_reward = calc_reward_to_go(reward_list)
 
-    agent.learn(batch_obs, batch_obs, batch_reward)
+    agent.learn(batch_obs, batch_action, batch_reward)
     if (i + 1) % 100 == 0:
-        total_reward = evaluate(env, agent, render=False)
-        logger.info('Test reward: {}'.format(total_reward))
+        total_reward = evaluate(env, agent, render=False) 
+        logger.info('Test reward: {}'.format(total_reward)
